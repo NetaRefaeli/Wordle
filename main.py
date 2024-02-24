05d39ea980c01e14f4a48f0dd81cdca526e78d51
NetaRefaeli <Rafaelineta@gmail.com>
2024-02-24 16:47:11 +0200

    main update

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
   4. יש לך 6 ניחושים
   ''')
   # play
+  wordle_ui.ask_user_if_to_play()
   while wordle_ui.ask_user_if_to_play()==True and wordle_game.is_game_running()==True:
     wordle_ui.get_user_guess()
 
